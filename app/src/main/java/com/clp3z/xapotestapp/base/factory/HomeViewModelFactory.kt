3606e5e40abddb808f38e144f0c58378cc1fbc51
Clelia <clelia.clp3z@gmail.com>
2021-02-26 13:14:54 +0100

    - Created temporal HomeViewModelFactory - Created temporal RepositoryViewModelFactory

--- app/src/main/java/com/clp3z/xapotestapp/base/factory/HomeViewModelFactory.kt
+++ app/src/main/java/com/clp3z/xapotestapp/base/factory/HomeViewModelFactory.kt
@@ -3,20 +3,23 @@ package com.clp3z.xapotestapp.base.factory
 import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.clp3z.xapotestapp.sections.repository.RepositoryModel
-import com.clp3z.xapotestapp.sections.repository.RepositoryViewModel
+import com.clp3z.xapotestapp.sections.home.HomeModel
+import com.clp3z.xapotestapp.sections.home.HomeViewModel
 
+/**
+ * Created by Clelia López on 02/26/21
+ */
 class HomeViewModelFactory(
     private val application: Application,
-    private val repositoryModel: RepositoryModel
+    private val homeModel: HomeModel
 ):
     ViewModelProvider.Factory {
 
     @Suppress("unchecked_cast")
     override fun <T: ViewModel?> create(modelClass: Class<T>): T {
         return when (modelClass) {
-            RepositoryViewModel::class.java ->
-                RepositoryViewModel(application, repositoryModel) as T
+            HomeViewModel::class.java ->
+                HomeViewModel(application, homeModel) as T
 
             else ->
                 throw IllegalArgumentException("ViewModelFactory: Unknown ViewModel class")
--- app/src/main/java/com/clp3z/xapotestapp/base/factory/RepositoryViewModelFactory.kt
+++ app/src/main/java/com/clp3z/xapotestapp/base/factory/RepositoryViewModelFactory.kt
@@ -4,8 +4,11 @@ import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.clp3z.xapotestapp.sections.repository.RepositoryModel
-import com.clp3z.xapotestapp.sections.repository.RepositoryViewModelR
+import com.clp3z.xapotestapp.sections.repository.RepositoryViewModel
 
+/**
+ * Created by Clelia López on 02/26/21
+ */
 class RepositoryViewModelFactory(
     private val application: Application,
     private val repositoryModel: RepositoryModel
@@ -14,13 +17,12 @@ class RepositoryViewModelFactory(
 
     @Suppress("unchecked_cast")
     override fun <T: ViewModel?> create(modelClass: Class<T>): T {
-
-
         return when (modelClass) {
+            RepositoryViewModel::class.java ->
+                RepositoryViewModel(application, repositoryModel) as T
 
-            RepositoryViewModelR::class.java -> RepositoryViewModelR(application, repositoryModel) as T
-
-            else -> throw IllegalArgumentException("ViewModelFactory: Unknown ViewModel class")
+            else ->
+                throw IllegalArgumentException("ViewModelFactory: Unknown ViewModel class")
         }
     }
 }
